c3f7a6a9d505a4d315380399a325c3f6a2c17fa2
faridanaksoleh <itspuyd@gmail.com>
2025-10-03 15:24:03 +0700

    feat: install NiceAdmin template & setup master layout

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
-use App\Livewire\About;
-use App\Livewire\Contact;
-use App\Livewire\Test;
+use App\Livewire\Dashboard;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', Test::class);
-
-Route::get('/about', About::class);
-
-Route::get('/contact', Contact::class);
+Route::get('/dashboard', Dashboard::class);
